8078929b33480e76a2deab7b4f30d21c7fc2599a
Michael Röder <michael.roeder@uni-paderborn.de>
2024-02-21 15:01:42 +0100

    Added script to run init script.

---
+++ SAIL-winter-school-2024/config/run-storage-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Runs the initialization script for the Storage of the HOBBIT Platform.
+VOS_CONTAINER_ID=$(docker ps -aqf "name=vos")
+VOS_CONTAINER_NUMBER=$(docker ps -aqf "name=vos" | wc -l)
+if [ "$VOS_CONTAINER_NUMBER" -eq "1" ]; then
+  docker exec -it $VOS_CONTAINER_ID bash /opt/virtuoso-opensource/database/storage-init.sh
+else
+  echo "Can not determine vos container name..."
+  echo "Use docker ps to determine vos container name and execute the following command manually:"
+  echo "docker exec -it yourvoscontainername bash ./storage-init.sh"
+fi
